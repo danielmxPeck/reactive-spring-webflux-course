bbf54d2d543262f0d5534a3970ac01fb90ce9f62
danielmxPeck <danielxuan98@gmail.com>
2025-01-03 00:25:21 +0800

    Complete lesson 16

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,36 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    Flux<String> namesFlux(){
+         return Flux.fromIterable(List.of("Adam", "Anna", "Jack", "Jenny"));
+    }
+
+    Mono<String> nameMono(){
+        return Mono.just("Adam");
+    }
+
+    public static void main(String[] args){
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.namesFlux()
+            .subscribe(
+                name ->
+                {
+                    System.out.println("Name is: " + name);
+                }
+            );
+
+        fluxAndMonoGeneratorService.nameMono().subscribe(
+            name ->
+            {
+                System.out.println("Mono name is: " + name);
+            }
+        );
+    }
 }
